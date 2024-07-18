7f4428a984623b79f632e1f375dbd269d75ef5f4
Soomin Kim <96629550+soominKimme@users.noreply.github.com>
2024-07-18 22:19:55 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
+// #2.1 Mouse Painting
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = 800;
 canvas.height = 800;
-
 ctx.lineWidth = 2;
 
-const colors = [
-    "#f03e3e",
-    "#e64980",
-    "#be4bdb",
-    "#7950f2",
-    "#4c6ef5",
-    "#228be6",
-    "#15aabf",
-    "#12b886",
-    "#40c057",
-    "#82c91e",
-    "#fab005",
-    "#fd7e14"
-]
+let isPainting = false;
 
-function onClick(event) {
-  ctx.beginPath();
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+function onMove(event){
+    if(isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("mousemove", onClick);
+function startPainting(){
+    isPainting = true;
+}
+
+function finishPainting(){
+    isPainting = false;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", finishPainting);
+canvas.addEventListener("mouseleave", finishPainting);
+
+// document.addEventListener("mouseup", finishPainting);
+
